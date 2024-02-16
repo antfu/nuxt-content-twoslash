58603b008c029979a41f6262caa6a7a05cda69f3
Anthony Fu <anthonyfu117@hotmail.com>
2024-02-16 13:23:24 +0100

    feat(cli): support watch

--- src/cli.ts
+++ src/cli.ts
@@ -11,6 +11,7 @@ import { join, relative, resolve } from 'pathe'
 import c from 'picocolors'
 import remarkParse from 'remark-parse'
 import { visit } from 'unist-util-visit'
+import type { ShikiTransformer } from 'shiki'
 import { codeToHast, getSingletonHighlighter } from 'shiki'
 import { cac } from 'cac'
 import { createTransformer } from './runtime/transformer'
@@ -27,10 +28,62 @@ export interface VerifyOptions {
   rootDir?: string
   buildDir?: string
   contentDir?: string
+  watch?: boolean
+}
+
+let currentFile = ''
+let currnetLine = 0
+const realCwd = process.cwd()
+
+const errors: TwoslashVerifyError[] = []
+
+async function runForFile(transformer: ShikiTransformer, filepath: string) {
+  currentFile = filepath
+  const content = await fs.readFile(filepath, 'utf-8')
+
+  if (!content.includes('twoslash'))
+    return
+
+  const file = unified()
+    .use(remarkParse)
+    .parse(content)
+
+  const codeBlocks: Code[] = []
+
+  visit(file, 'code', (node) => {
+    if (!node.meta?.match(/\btwoslash\b/))
+      return
+    codeBlocks.push(node)
+  })
+
+  if (!codeBlocks.length)
+    return
+
+  console.log(`Checking ${relative(realCwd, filepath)} (${codeBlocks.length} twoslash blocks)`)
+
+  for (const block of codeBlocks) {
+    currnetLine = block.position?.start?.line || 0
+    try {
+      await codeToHast(block.value, {
+        lang: block.lang as any,
+        theme: 'min-dark',
+        transformers: [transformer],
+        meta: {
+          __raw: block.meta as any,
+        },
+      })
+    }
+    catch (error) {
+      errors.push({
+        file: currentFile,
+        line: block.position?.start?.line || 0,
+        error,
+      })
+    }
+  }
 }
 
 export async function verify(options: VerifyOptions = {}) {
-  const realCwd = process.cwd()
   const root = options.rootDir || process.cwd()
   const {
     resolveNuxt = existsSync(join(root, 'nuxt.config.js')) || existsSync(join(root, 'nuxt.config.ts')),
@@ -64,10 +117,6 @@ export async function verify(options: VerifyOptions = {}) {
     followSymbolicLinks: false,
   })
 
-  let currentFile = ''
-  let currnetLine = 0
-  const errors: TwoslashVerifyError[] = []
-
   const transformer = await createTransformer(
     twoslashOptions,
     typeDecorations,
@@ -96,68 +145,48 @@ export async function verify(options: VerifyOptions = {}) {
   console.log(markdownFiles.map(f => `  - ${relative(realCwd, f)}`).join('\n'))
   console.log('')
 
-  for (const path of markdownFiles) {
-    currentFile = path
-    const content = await fs.readFile(path, 'utf-8')
-
-    if (!content.includes('twoslash'))
-      continue
-
-    const file = unified()
-      .use(remarkParse)
-      .parse(content)
-
-    const codeBlocks: Code[] = []
-
-    visit(file, 'code', (node) => {
-      if (!node.meta?.match(/\btwoslash\b/))
-        return
-      codeBlocks.push(node)
-    })
-
-    if (!codeBlocks.length)
-      continue
-
-    console.log(`Checking ${relative(realCwd, path)} (${codeBlocks.length} twoslash blocks)`)
-
-    for (const block of codeBlocks) {
-      currnetLine = block.position?.start?.line || 0
-      try {
-        await codeToHast(block.value, {
-          lang: block.lang as any,
-          theme: 'min-dark',
-          transformers: [transformer],
-          meta: {
-            __raw: block.meta as any,
-          },
-        })
-      }
-      catch (error) {
-        errors.push({
-          file: currentFile,
-          line: block.position?.start?.line || 0,
-          error,
-        })
-      }
-    }
-  }
+  for (const path of markdownFiles)
+    await runForFile(transformer, path)
 
   console.log()
 
   if (errors.length) {
-    console.error(c.red('Twoslash verification failed'))
-    for (const error of errors) {
-      console.error(c.yellow(`\n----------\nError in ${relative(realCwd, error.file)}:${error.line}`))
-      console.error(c.red(String(error.error)))
-    }
-    console.error(c.yellow('----------'))
-    console.error(c.red(`\nTwoslash verification failed with ${errors.length} errors.`))
-    console.error(c.red(errors.map(e => `  - ${relative(realCwd, e.file)}:${e.line}`).join('\n')))
-    process.exit(1)
+    printErrors()
+    if (!options.watch)
+      process.exit(1)
   }
   else {
     console.log(c.green('Twoslash verification passed'))
   }
+
+  if (options.watch) {
+    const chokidar = await import('chokidar')
+    const watcher = chokidar.watch(markdownFiles, {
+      ignoreInitial: true,
+      ignorePermissionErrors: true,
+    })
+
+    watcher.on('change', async (path) => {
+      console.log(c.yellow('File changed'), relative(realCwd, path))
+      errors.length = 0
+      await runForFile(transformer, path)
+      if (errors.length)
+        printErrors()
+      else
+        console.log(c.green('Twoslash verification passed for ') + relative(realCwd, path))
+    })
+  }
+}
+
+function printErrors() {
+  console.error(c.red('Twoslash verification failed'))
+  for (const error of errors) {
+    console.error(c.yellow(`\n----------\nError in ${relative(realCwd, error.file)}:${error.line}`))
+    console.error(c.red(String(error.error)))
+  }
+  console.error(c.yellow('----------'))
+  console.error(c.red(`\nTwoslash verification failed with ${errors.length} errors.`))
+  console.error(c.red(errors.map(e => `  - ${relative(realCwd, e.file)}:${e.line}`).join('\n')))
 }
 
 const cli = cac('nuxt-content-twoslash')
@@ -167,6 +196,7 @@ cli.command('verify', 'Verify twoslash code blocks in markdown files')
   .option('--content-dir <dir>', 'The content directory of the Nuxt project')
   .option('--root-dir <dir>', 'The root directory of the Nuxt project')
   .option('--resolve-nuxt', 'Resolve Nuxt project', { default: false })
+  .option('-w, --watch', 'Watch files', { default: false })
   .action(args => verify(args))
 
 cli.command('')
