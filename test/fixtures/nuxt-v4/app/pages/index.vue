63e516dad545e95e5cc646cb012fd271cc9a4c91
Daniel Roe <daniel@roe.dev>
2026-02-13 13:13:14 +0000

    test: add failing test for nuxt v4 with type contexts

---
+++ test/fixtures/nuxt-v4/app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+</script>
+
+<template>
+  <ContentRenderer v-if="home" :value="home" />
+  <div v-else>
+    Home not found
+  </div>
+</template>
